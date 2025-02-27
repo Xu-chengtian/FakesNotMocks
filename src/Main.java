447dc9aa5fcd1ec2e378b7b550c42f6ed27dc2b0
Xu-chengtian <xuchengtian0702@163.com>
2025-02-26 18:26:53 -0800

    init commit, add fake exchange in unit test

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+class FakeExchange implements Exchange {
+    @Override
+    public float rate(String origin, String target) {
+        if (origin.equalsIgnoreCase("USD") && target.equalsIgnoreCase("Euro")) {
+            return 0.8f;
+        }
+        return 1.0f;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         // Unit Test for Cash class
 
         // Test setup
-        Exchange exchange = new NYSE("someAccessKey");
+        Exchange exchange = new FakeExchange();
         Cash dollar = new Cash(exchange, 100);
         System.out.println("Dollar: " + dollar.toString());
 
